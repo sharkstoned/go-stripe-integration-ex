f1867a81a55a6d4019c4b7780326f3ae9bad4d2a
Stanislav Rastchinkin <stanlyraschinkin@gmail.com>
2024-12-22 16:20:49 +0100

    basic app

---
+++ cmd/web/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+	"time"
+)
+
+const version = "1.0.0"
+const cssVersion = "1"
+
+type config struct {
+	port int
+	env  string
+	api  string
+	db   struct {
+		dsn string
+	}
+	stripe struct {
+		secret string
+		key    string
+	}
+}
+
+type application struct {
+	config        config
+	infoLog       *log.Logger
+	errorLog      *log.Logger
+	templateCache map[string]*template.Template
+	version       string
+}
+
+func (app *application) serve() error {
+	server := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+
+	app.infoLog.Println("Listening on port %d, env %s", app.config.port, app.config.env)
+
+	return server.ListenAndServe()
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "Define a port to run the applicatoin on")
+	flag.StringVar(&cfg.env, "env", "dev", "Execution environment {dev|prod}")
+	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "API URL")
+
+	flag.Parse()
+
+	cfg.stripe.key = os.Getenv("STRIPE_KEY")
+	cfg.stripe.secret = os.Getenv("STRIPE_SECRET")
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	templateCache := make(map[string]*template.Template)
+
+	app := &application{
+		config:        cfg,
+		infoLog:       infoLog,
+		errorLog:      errorLog,
+		templateCache: templateCache,
+		version:       version,
+	}
+
+	err := app.serve()
+
+	if err != nil {
+		app.errorLog.Println(err)
+		log.Fatal(err)
+	}
+}
